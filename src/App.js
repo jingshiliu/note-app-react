3729747cb571c54bc25a4b0de1865bef7c6af5c9
Jingshi Liu <jingshiliu07@gmail.com>
2022-09-26 22:04:48 -0400

    Commit to continue work on iMac

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import React, {useState} from "react"
+import Sidebar from "./components/Sidebar"
+import Editor from "./components/Editor"
+import {nanoid} from "nanoid"
+import {data, saveDataToLocalStorage} from "./data";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+export default function App() {
+    const [notes, setNotes] = useState(data)
+    const [currentNote, setCurrentNote] = useState()
+
+    function updateNote(){
+
+    }
+
+    function createNote(newNote){
+        setNotes({...notes, })
+    }
+
+    return (
+        <div className='App'>
+            <Sidebar />
+            <Editor />
+        </div>
+    )
 }
-
-export default App;
