015820bc29120d78e41d469cfa509f15ecde3fad
Jingshi Liu <jingshiliu07@gmail.com>
2022-09-28 22:19:07 -0400

    Added styles and other features Next step make text editor work

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function TextEditor({textContent, setTextContent, updateNote}){
     return (
-        <div className='TextEditor form-control'
+        <div className='form-control TextEditor'
              onInput={e => {
                  setTextContent(e.target.innerText)
                  updateNote(e.target.innerText)
