78c16724d9cb4721685f93d2c081ac0924c34ad0
Jingshi Liu <jingshiliu07@gmail.com>
2022-09-29 13:52:56 -0400

    Tried React-ContentEditable. Doesn't work as expected. Moving to Draft.js next commit

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -6,7 +6,6 @@ import TextEditor from "./TextEditor";
 
 export default function Editor({currentNoteID, notes, updateNote}) {
     const [textContent, setTextContent] = useState(notes.find(note => note.id === currentNoteID).body)
-
     const textEditorProps = {textContent, setTextContent, updateNote}
     return (
         <div className='Editor'>
--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
+import ContentEditable from "react-contenteditable";
 
 export default function TextEditor({textContent, setTextContent, updateNote}){
+    // return (
+    //     <div className='TextEditor form-control'
+    //          onInput={e => {
+    //              setTextContent(e.target.innerText)
+    //              updateNote(e.target.innerText)
+    //          }}
+    //          contentEditable={true}>
+    //         {textContent}
+    //     </div>
+    // )
+    const ref = useRef(textContent)
+
     return (
-        <div className='form-control TextEditor'
-             onInput={e => {
-                 setTextContent(e.target.innerText)
-                 updateNote(e.target.innerText)
-             }}
-             contentEditable={true}>
-            {textContent}
-        </div>
+        <ContentEditable
+            className='TextEditor form-control'
+            innerRef={ref}
+            html={textContent} // innerHTML of the editable div
+            disabled={false}       // use true to disable editing
+            onChange={e =>{
+                setTextContent(e.target.value)
+                updateNote(e.target.value)
+            }} // handle innerHTML change
+        />
     )
 }
